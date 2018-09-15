de3d4650253c2d7365d7ef8428bbe2bbf62f0333
Van Pho Nguyen <vanpho02@gmail.com>
2018-09-15 17:18:30 +0700

    validator

--- src/app/components/form-sign-up/form-sign-up.component.ts
+++ src/app/components/form-sign-up/form-sign-up.component.ts
@@ -9,7 +9,7 @@ import { FormGroup, FormControl, Validators } from '@angular/forms';
 export class FormSignUpComponent implements OnInit {
 
   formSignUp = new FormGroup({
-    email: new FormControl('', [Validators.required, Validators.email]),
+    email: new FormControl('', [Validators.required, Validators.email, gmail]),
     name: new FormControl('', [Validators.required, Validators.minLength(4)]),
     city: new FormControl('Sai Gon'),
     zip: new FormControl('70000'),
@@ -25,7 +25,13 @@ export class FormSignUpComponent implements OnInit {
     if (emailControl.untouched || emailControl.valid) return '';
     if (emailControl.errors.required) return 'Bạn cần nhập email';
     if (emailControl.errors.email) return 'Email không đúng định dạng';
+    if (emailControl.errors.gmail) return 'Email phải là gmail';
   }
 
   signUp() { alert(JSON.stringify(this.formSignUp.value)); }
+}
+
+function gmail(control: FormControl) {
+  if ((control.value as string).endsWith('@gmail.com')) return null;
+  return { gmail: true };
 }
